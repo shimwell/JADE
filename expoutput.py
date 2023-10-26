384a9dcf02d87f3ffc0af2f5761cec932a12960e
AlbertoBittes <116062815+AlbertoBittes@users.noreply.github.com>
2023-10-26 16:48:43 +0200

    solving issue in Github

--- expoutput.py
+++ expoutput.py
@@ -1695,7 +1695,7 @@ class MultipleSpectrumOutput(SpectrumOutput):
                        unit, xlabel, self.testname, group_num=group,
                        add_labels=group_lab, mult_factors=mult_factors)
         img_path = plot.plot('Experimental points group')
-        atlas.doc.add_heading(self.testname, level=1)
+        atlas.doc.add_heading(title, level=1)
         atlas.insert_img(img_path)
         img_path = plot.plot('Experimental points group CE')
         atlas.doc.add_heading(title + ' C/E', level=1)
